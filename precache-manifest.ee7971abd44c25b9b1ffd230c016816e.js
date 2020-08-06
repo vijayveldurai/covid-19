6bb088855dd3351e1a80407083cc14a06931dad5
vijayveldurai <vijaykumar.veldurai@gmail.com>
2020-08-06 18:53:10 +0530

    added files

---
+++ precache-manifest.ee7971abd44c25b9b1ffd230c016816e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d060510774cba69175beac4b6c02f9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ad662ccf5b81b1aeca",
+    "url": "/static/css/main.005e0240.chunk.css"
+  },
+  {
+    "revision": "c9af825f9853a977d47e",
+    "url": "/static/js/2.af2b171a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.af2b171a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ad662ccf5b81b1aeca",
+    "url": "/static/js/main.2ac3843b.chunk.js"
+  },
+  {
+    "revision": "c7da66034fe61b79a719",
+    "url": "/static/js/runtime-main.20e25257.js"
+  }
+]);
