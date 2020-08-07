b195f7a30ff5bdc4b254b321007050cbb4895af1
vijayveldurai <vijaykumar.veldurai@gmail.com>
2020-08-07 16:18:25 +0530

    new changes

---
+++ precache-manifest.3c551ff38a49948f020e8de943c95fe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d583f74a68180c6d032b3cc556c7d2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e61fbc98ddaf3a58146",
+    "url": "./static/css/main.005e0240.chunk.css"
+  },
+  {
+    "revision": "c9af825f9853a977d47e",
+    "url": "./static/js/2.af2b171a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.af2b171a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e61fbc98ddaf3a58146",
+    "url": "./static/js/main.8a5a4ebd.chunk.js"
+  },
+  {
+    "revision": "c1a40504d53cada04a56",
+    "url": "./static/js/runtime-main.fc465907.js"
+  }
+]);
